7c273c035d084ffa24b09c2f23886d3fb44cb7db
rajputdevyansh <devyanshrajput50@gmail.com>
2023-04-19 06:26:17 +0530

    landing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 'use strict';
+
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
@@ -39,3 +40,29 @@ const activeEl = function () {
 }
 
 window.addEventListener("scroll", activeEl);
+
+/* Button hover ripple effect */
+const buttons = document.querySelectorAll("[data-btn]");
+
+const buttonHoverRipple = function (event) {
+  this.style.setProperty("--top", `${event.offsetY}px`);
+  this.style.setProperty("--left", `${event.offsetX}px`);
+}
+
+addEventOnElements(buttons, "mousemove", buttonHoverRipple);
+
+/* Scroll reveal */
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const revealElementOnScroll = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    const isElementInsideWindow = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.1;
+
+    if (isElementInsideWindow) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", revealElementOnScroll);
+window.addEventListener("load", revealElementOnScroll);
