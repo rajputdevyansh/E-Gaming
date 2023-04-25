d57acd5263dfb2f64a1f5cde514dff218d1803ca
rajputdevyansh <devyanshrajput50@gmail.com>
2023-04-25 23:34:16 +0530

    commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+mongoose.connect('mongodb://0.0.0.0:27017/Game',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
