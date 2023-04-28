638e921eef15a58b6a9a6c47b304684fb70fb539
rajputdevyansh <devyanshrajput50@gmail.com>
2023-04-28 17:38:23 +0530

    commit

--- index.js
+++ index.js
@@ -1,42 +1,83 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://0.0.0.0:27017/GameSite";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+var express = require("express")
+var bodyParser = require("body-parser")
+var mongoose = require("mongoose")
 
-let upbtn = document.getElementById("upbtn");
-upbtn.addEventListener("click", (event) => {
-  event.preventDefault();
+const app = express()
 
-  client.connect(err => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("Connected successfully to server");
-      const db = client.db("GameSite");
-      const collection = db.collection("Users");
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
 
-      // Get the form data
-      const username = document.querySelector("#userH input").value;
-      const fullName = document.querySelector("#nameH input").value;
-      const email = document.querySelector("#emailH input").value;
-      const password = document.querySelector("#passH input").value;
+app.get("/",(req,res)=>{
+    res.set({
+        "Allow-access-Allow-Origin": '*'
+    })
+    return res.redirect('Home.html');
+  }).listen(3000);
+  
+  
+  console.log("Listening on PORT 3000");
 
-      // Insert the form data into the Users collection
-      collection.insertOne({
-        username: username,
-        fullName: fullName,
-        email: email,
-        password: password
-      }, (err, result) => {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log("Document inserted:", result);
-
-          // Redirect to a different page
-          window.location.href = "/success.html";
-        }
-        client.close();
-      });
-    }
-  });
+mongoose.connect('mongodb://0.0.0.0:27017/GameSite',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+var db = mongoose.connection;
+
+db.on('error',()=>console.log("Error in Connecting to Database"));
+db.once('open',()=>console.log("Connected to Database"))
+
+// Sign Up
+app.post("/sign_up",(req,res)=>{
+    var username =req.body.username;
+    var name = req.body.fullname;
+    var email = req.body.email;
+    var phonoNo = req.body.phone;
+    var password = req.body.password;
+
+    var data = {
+        "username": username,
+        "name": name,
+        "email" : email,
+        "phno": phonoNo,
+        "password" : password
+    }
+
+    db.collection('users').insertOne(data,(err,collection)=>{
+        if(err){
+            throw err;
+        }
+        console.log("Record Inserted Successfully");
+    });
+
+    return res.redirect('login.html')
+})
+
+// Login
+app.post("/login", async (request, response) => {
+    try {
+        const username = request.body.username;
+        const password = request.body.password;
+
+        const usermail = db.collection('users').findOne({ email: username }, (err, res) => {
+            if (res == null) {
+                response.send("Invalid information!❌❌❌! Please create account first");
+            }
+            else if (err) throw err;
+
+
+            if (res.password === password) {
+                return response.redirect('Homeafter.html');
+            }
+            else {
+                response.send("Invalid Password!❌❌❌");
+            }
+        });
+    }
+    catch (error) {
+        response.send("Invalid information❌");
+    }
+})
